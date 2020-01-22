f5f8c44261abe927ee0bcf694f6d8f10daccb847
Renato Alves <renato@alley.co>
2020-01-21 22:44:49 -0300

    Simplifying Blogs connection registration

--- src/Connection/BlogConnection.php
+++ src/Connection/BlogConnection.php
@@ -23,60 +23,40 @@ class BlogConnection {
 	public static function register_connections() {
 
 		/**
-		 * Register connection from RootQuery to blogs.
+		 * Register connection from RootQuery -> Blog (type).
 		 */
-		register_graphql_connection( self::get_connection_config() );
-	}
-
-	/**
-	 * This returns a RootQuery > blog connection config.
-	 *
-	 * @param array $args Array of arguments.
-	 *
-	 * @return array
-	 */
-	public static function get_connection_config( $args = [] ) {
-		$defaults = [
-			'fromType'       => 'RootQuery',
-			'toType'         => 'Blog',
-			'fromFieldName'  => 'blogs',
-			'connectionArgs' => self::get_connection_args(),
-			'resolveNode'    => function ( $id ) {
-				return Factory::resolve_blog_object( $id );
-			},
-			'resolve'        => function ( $source, array $args, AppContext $context, ResolveInfo $info ) {
-				return Factory::resolve_blogs_connection( $source, $args, $context, $info );
-			},
-		];
-
-		return array_merge( $defaults, $args );
-	}
-
-	/**
-	 * This returns the connection args for the Blogs connection.
-	 *
-	 * @return array
-	 */
-	public static function get_connection_args() {
-		return [
-			'type'        => [
-				'type'        => 'BlogOrderTypeEnum',
-				'description' => __( 'The order in which blogs should be returned.', 'wp-graphql-buddypress' ),
-			],
-			'search'      => [
-				'type'        => 'String',
-				'description' => __( 'Search term(s) to retrieve matching blogs for.', 'wp-graphql-buddypress' ),
-			],
-			'userId'      => [
-				'type'        => 'Int',
-				'description' => __( 'ID of the user whose blogs user can post to.', 'wp-graphql-buddypress' ),
-			],
-			'include'     => [
-				'type'        => [
-					'list_of' => 'Int',
+		register_graphql_connection(
+			[
+				'fromType'       => 'RootQuery',
+				'toType'         => 'Blog',
+				'fromFieldName'  => 'blogs',
+				'connectionArgs' => [
+					'type'        => [
+						'type'        => 'BlogOrderTypeEnum',
+						'description' => __( 'The order in which blogs should be returned.', 'wp-graphql-buddypress' ),
+					],
+					'search'      => [
+						'type'        => 'String',
+						'description' => __( 'Search term(s) to retrieve matching blogs for.', 'wp-graphql-buddypress' ),
+					],
+					'userId'      => [
+						'type'        => 'Int',
+						'description' => __( 'ID of the user whose blogs user can post to.', 'wp-graphql-buddypress' ),
+					],
+					'include'     => [
+						'type'        => [
+							'list_of' => 'Int',
+						],
+						'description' => __( 'Ensure result set includes Blogs with specific IDs.', 'wp-graphql-buddypress' ),
+					],
 				],
-				'description' => __( 'Ensure result set includes Blogs with specific IDs.', 'wp-graphql-buddypress' ),
-			],
-		];
+				'resolveNode' => function ( $id ) {
+					return Factory::resolve_blog_object( $id );
+				},
+				'resolve' => function ( $source, array $args, AppContext $context, ResolveInfo $info ) {
+					return Factory::resolve_blogs_connection( $source, $args, $context, $info );
+				},
+			]
+		);
 	}
 }
