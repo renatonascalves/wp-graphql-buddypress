f11c1c3292c1efa55fc251bb5fb04317b893300d
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2021-04-08 19:11:32 -0300

    Unit tests: improving the members tests

--- src/Type/Object/MemberType.php
+++ src/Type/Object/MemberType.php
@@ -11,6 +11,7 @@ namespace WPGraphQL\Extensions\BuddyPress\Type\Object;
 use GraphQL\Error\UserError;
 use WPGraphQL\Extensions\BuddyPress\Data\Factory;
 use WPGraphQL\Model\User;
+use BP_User_Query;
 
 /**
  * MemberType Class.
@@ -37,11 +38,7 @@ class MemberType {
 				'resolve'     => function ( User $source ) {
 					$types = bp_get_member_type( $source->userId ?? 0, false );
 
-					if ( empty( $types ) ) {
-						return null;
-					}
-
-					return $types;
+					return $types ?? null;
 				},
 			]
 		);
@@ -59,11 +56,7 @@ class MemberType {
 
 					$mention_name = bp_activity_get_user_mentionname( $source->userId ?? 0 );
 
-					if ( empty( $mention_name ) ) {
-						return null;
-					}
-
-					return $mention_name;
+					return $mention_name ?? null;
 				},
 			]
 		);
@@ -84,11 +77,94 @@ class MemberType {
 
 		register_graphql_field(
 			self::$type_name,
+			'latestUpdate',
+			[
+				'type'        => 'String',
+				'description' => __( 'The content of the latest activity posted by the member.', 'wp-graphql-buddypress' ),
+				'args'        => [
+					'format' => [
+						'type'        => 'ContentFieldFormatEnum',
+						'description' => __( 'Format of the field output', 'wp-graphql-buddypress' ),
+					],
+				],
+				'resolve'     => function ( User $source ) {
+					if ( ! bp_is_active( 'activity' ) ) {
+						throw new UserError( __( 'The Activity component needs to be active to use this field.', 'wp-graphql-buddypress' ) );
+					}
+
+					// Get the member with BuddyPress extra data.
+					$member_query = new BP_User_Query(
+						[
+							'user_ids'        => [ $source->userId ],
+							'populate_extras' => true,
+						]
+					);
+
+					$member = reset( $member_query->results );
+
+					if ( empty( $member->latest_update ) ) {
+						return null;
+					}
+
+					$activity_data = maybe_unserialize( $member->latest_update );
+
+					if ( empty( $activity_data['content'] ) ) {
+						return null;
+					}
+
+					if ( isset( $args['format'] ) && 'raw' === $args['format'] ) {
+						return $activity_data['content'];
+					}
+
+					return apply_filters( 'bp_get_activity_content', $activity_data['content'] );
+				},
+			]
+		);
+
+		register_graphql_field(
+			self::$type_name,
+			'totalFriendCount',
+			[
+				'type'        => 'Int',
+				'description' => __( 'Total number of friends for the member.', 'wp-graphql-buddypress' ),
+				'resolve'     => function ( User $source ) {
+
+					if ( ! bp_is_active( 'friends' ) ) {
+						throw new UserError( __( 'The Friends component needs to be active to use this field.', 'wp-graphql-buddypress' ) );
+					}
+
+					// Get the member with BuddyPress extra data.
+					$member_query = new BP_User_Query(
+						[
+							'user_ids'        => [ $source->userId ],
+							'populate_extras' => true,
+						]
+					);
+
+					$member = reset( $member_query->results );
+
+					if ( empty( $member->total_friend_count ) ) {
+						return null;
+					}
+
+					return absint( $member->total_friend_count );
+				},
+			]
+		);
+
+		register_graphql_field(
+			self::$type_name,
 			'attachmentAvatar',
 			[
 				'type'        => 'Attachment',
 				'description' => __( 'Attachment Avatar of the member.', 'wp-graphql-buddypress' ),
 				'resolve'     => function ( User $source ) {
+
+					// Bail early, if disabled.
+					if ( false === buddypress()->avatar->show_avatars ) {
+						return null;
+					}
+
 					return Factory::resolve_attachment( $source->userId ?? 0 );
 				},
 			]
