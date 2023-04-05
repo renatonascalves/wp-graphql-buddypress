47dbf8e68cde0d379ae13256e7f1fa4fd6b529d7
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2023-04-05 15:29:15 -0300

    Github: test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,6 +15,9 @@ require_once dirname( __FILE__, 2 ) . '/vendor/wp-phpunit/wp-phpunit/includes/fa
 	})
 	->loaded(
 		function() {
+			define( 'WP_TESTS_CONFIG_FILE_PATH', '/tmp/wordpress/wp-tests-config.php' );
+			define( 'WP_TESTS_CONFIG_PATH', '/tmp/wordpress/wp-tests-config.php' );
+
 			// Load plugins.
 			require_once BP_TESTS_DIR . '/includes/loader.php';
 			require_once dirname( __FILE__, 3 ) . '/wp-graphql/wp-graphql.php';
