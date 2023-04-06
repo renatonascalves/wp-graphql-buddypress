dc62343e92d4a397d9450f25dae3395b5240b1c0
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2023-04-05 22:36:39 -0300

    Github: tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,10 +11,9 @@ require_once dirname( __FILE__, 2 ) . '/vendor/wp-phpunit/wp-phpunit/includes/fa
 
 \Mantle\Testing\manager()
 	->before( function() {
-		define( 'WP_TESTS_CONFIG_FILE_PATH', '/tmp/wordpress-tests-lib/wp-tests-config.php' );
+		define( 'WP_TESTS_CONFIG_FILE_PATH', dirname( __FILE__ ) . '/includes/wp-tests-config.php' );
 		define( 'WP_TESTS_CONFIG_PATH', WP_TESTS_CONFIG_FILE_PATH );
 
-		require_once dirname( __FILE__, 2 ) . '/vendor/wp-phpunit/wp-phpunit/__loaded.php';
 		require_once dirname( __FILE__ ) . '/includes/define-constants.php';
 	})
 	->loaded(
---
+++ tests/includes/wp-tests-config.php
@@ -0,0 +1,61 @@
+<?php // phpcs:disable
+
+/* Path to the WordPress codebase you'd like to test. Add a forward slash in the end. */
+define( 'ABSPATH', dirname( __FILE__, 6 ) . '/' );
+
+/*
+ * Path to the theme to test with.
+ */
+define( 'WP_DEFAULT_THEME', 'twentytwenty' );
+
+/*
+ * Test with multisite enabled.
+ * Alternatively, use the tests/phpunit/multisite.xml configuration file.
+ */
+// define( 'WP_TESTS_MULTISITE', true );
+
+// Test with WordPress debug mode (default).
+define( 'WP_DEBUG', true );
+
+// ** MySQL settings ** //
+
+/*
+ * This configuration file will be used by the copy of WordPress being tested.
+ * wordpress/wp-config.php will be ignored.
+ *
+ * WARNING WARNING WARNING!
+ * These tests will DROP ALL TABLES in the database with the prefix named below.
+ * DO NOT use a production database or one that is shared with something else.
+ */
+
+define( 'DB_NAME', 'wordpress_unit_tests' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'root' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ */
+define('AUTH_KEY',         'ru~EW? DN2Y-4~d@ S4$.w2]i-sfJ8hHi+0-* bIj%7j!Xg=p5Aap&2K#bTS5}It');
+define('SECURE_AUTH_KEY',  'aIza+%`qW=DYdFC|zW:R|g5cR;8DUuJcSn}u@%GWnc6vp}N8rac.ezr[-pSb;jh<');
+define('LOGGED_IN_KEY',    'ohzNHv@qON]fNZa=$IVzcAFf.G-}1A?uTu*|bT+ocUHdn+mHd+-=AU P]sEA,8*G');
+define('NONCE_KEY',        'Xm`3yu{CDIGlyeUL0qOae3n-REPjO+/IHN:=f_]7a|N5mz1S.hnKYiGg,*Beb1U6');
+define('AUTH_SALT',        '~bos&4+*ceyTR,tNxckFTWTD2]DJ4f_1s^xPl4a>Zig+fEs104@RauaE/!k=xEi4');
+define('SECURE_AUTH_SALT', '7!nVL!+Bj%gQPm:Q|OaOqqD@%Q&.t!bOLJ)HWB#^xs;n<ngEj>d,@:3bzs4xGd1m');
+define('LOGGED_IN_SALT',   'tt33 :89iL+)6pXVzm&?zu#W(5Ir2Tj!t7@qAr,#.~w]Il@]-#Z<S~F}W/:3/.]X');
+define('NONCE_SALT',       'p8lEcu3nR35=3j4=>:LkVL+tP!|NeD07Q8(+REC^pg/$V=4|FuL?x$G|XRz%mliB');
+
+$table_prefix = 'wptests_';   // Only numbers, letters, and underscores please!
+
+define( 'WP_TESTS_DOMAIN', 'example.org' );
+define( 'WP_TESTS_EMAIL', 'admin@example.org' );
+define( 'WP_TESTS_TITLE', 'Test Site' );
+
+define( 'WP_PHP_BINARY', 'php' );
+
+define( 'WPLANG', '' );
